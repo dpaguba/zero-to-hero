d7671f708c22ec4986679dcaacfe39579ff97e55
dpaguba <88382171+dpaguba@users.noreply.github.com>
2022-01-21 10:16:35 +0100

    First program on Python. In future i need to add error handlers to make it more stable

---
+++ number-guessing-game.py
@@ -0,0 +1,45 @@
+from random import randint
+
+str_answer = input("Welcome in number guessing game!\nDo you want to play? y/n ")
+
+
+def play():
+    print("You cant quit the game anytime you want, just enter q")
+    str_ans = "y"
+
+    while str_ans == "y":
+        print("I remembered a number from 0 to 100. Try to guess it!")
+
+        number_to_guess = randint(0, 100)
+        int_attempt = 0
+        suggested_number = -1
+
+        while suggested_number != number_to_guess:
+            suggested_number = input("Your suggested number: ")
+
+            if suggested_number == "q":
+                break
+            elif int(suggested_number) == number_to_guess:
+                print("You guessed the number!")
+                print("Total attempts: ", int_attempt + 1)
+                break
+            else:
+                if int(suggested_number) < number_to_guess:
+                    print("The number I remembered is bigger")
+                else:
+                    print("The number I remembered is smaller")
+                int_attempt += 1
+
+        if suggested_number == "q":
+            print("Game was stopped!\n")
+            break
+        else:
+            str_ans = input("Do you want to play one more time? y/n ")
+
+
+if str_answer == "y":
+    play()
+elif str_answer == "n":
+    print("Game over!")
+else:
+    print("Incorrect input!")
