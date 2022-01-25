ea05be66b36d7d41bbd69fdababf9789a158681a
dpaguba <88382171+dpaguba@users.noreply.github.com>
2022-01-25 16:38:55 +0100

    read of data, convert data, and build graph

--- ploty-dash/exo_planets/app.py
+++ ploty-dash/exo_planets/app.py
@@ -2,39 +2,29 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-import requests
-# чтобы пробразовать запрос в датафрейм нужно импорт pandas
 import pandas as pd
-# график строим с помощью библиотеки plotly
+
 import plotly.express as px
-# для вывода прямо в бразуер
 import plotly.io as poi
-# результат plotly нужно выводить в браузер
+
+# вывод результата в браузер
 poi.renderers.default = "browser"
 
-# Из параметров в запросе у нас query (мы оставляем пустым, потому что хотим получить все данные)
-# и кол-во строк мы ограничим в 2000
-# результат запроса будет сохранен в переменную response как JSON-file
-response = requests.get("http://asterank.com/api/kepler?query={}&limit={2000}")
-# превращаем данные из json формата в dataframe
-df = pd.json_normalize(response.json())
+# Считывание и конвертация данных в dataframe
+df = pd.read_csv("asterank_exo.csv")
 
-# test
-# print(df.head())
-
+# описываем вид графика
 fig = px.scatter(df, x="RPLANET", y="TPLANET")
 
-# отобразить
-# fig.show()
-
-
-# инициализация
+# инициализация программы
 app = dash.Dash(__name__)
 
-# фронтенд приложения - описание как будет выглядеть приложение
-app.layout = html.Div("Hello")
+app.layout = html.Div([
+    html.H1("Hello Dash!"),
+    html.Div("Exoplanets chart"),
+    dcc.Graph(figure=fig)
+])
 
-
-# запуск приложения
+# запуск программы
 if __name__ == "__main__":
     app.run_server(debug=True)
