b54d844d5c52a4cd3fb54f3587b52f7a45c15f70
dpaguba <dpaguba@gmail.com>
2022-03-21 11:13:48 +0100

    analysis

--- plotly-dash/exo_planets/app.py
+++ plotly-dash/exo_planets/app.py
@@ -1,16 +1,14 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+
 # чтобы график мог реагировать на действия
 from dash.dependencies import Input, Output
-
+# библиотека пандас нужна для того чтобы данные с запроса превратить в dataframe
 import pandas as pd
-
+# библиотека для постройки графиков
 import plotly.express as px
-# import plotly.io as poi
-#
-# # вывод результата в браузер
-# poi.renderers.default = "browser"
+
 
 # Считывание и конвертация данных в dataframe
 df = pd.read_csv("asterank_exo.csv")
@@ -33,13 +31,17 @@ rplanet_selector = dcc.RangeSlider(
 # инициализация программы
 app = dash.Dash(__name__)
 
+# внешний вид приложения. Тут использую dcc html
 app.layout = html.Div([
     html.H1("Hello Dash!"),
     html.Div("Select planet main semi-axis range"),
     html.Div(rplanet_selector, style={"width": "500px", "margin-left": "auto", "margin-right": "auto",
                                       "margin-top": "10px", "margin-bottom": "20px"}),
     html.Div("Planet Temperature ~ Distance to the Star"),
-    dcc.Graph(id="graph", figure=fig)
+    # просто вывести график
+    # dcc.Graph(figure=fig)
+    # чтобы график был responsive
+    dcc.Graph(id="responsive-graph", figure=fig)
 ],
     style={"margin-left": "80px",
            "margin-right": "80px",
@@ -47,10 +49,21 @@ app.layout = html.Div([
 
 # запуск программы
 if __name__ == "__main__":
+    # запуск сервера в тестовом режиме
     app.run_server(debug=True)
 
+# оболочка над функцией, для динамической работы с переменными
+@app.callback(
+    # первый параметр - это куда мы будем передавать данные, а второй параметр это форма показа(форма сущности)
+    Output(component_id="responsive-graph", component_property="figure"),
+    # первый параметр - откуда мы берем данные, второй параметр говорит конкретно, что мы будем передевать
+    Input(component_id="range-slider", component_property="value")
+)
+#     value передается и мы назыаем это radius_range
+def update_graph(radius_range):
 
-# главная полуось планеты, они движуться по элиптической оси и эти данные говорят нам о том насколько далео в среднем
+
+#главная полуось планеты, они движуться по элиптической оси и эти данные говорят нам о том насколько далео в среднем
 # эта планета находится от своей звезды и мы строим зависимость меджу расстоянием и температурой
 # x это температура, а y это расстояние
 # задание добавить фильтр на основе радиуса планеты
