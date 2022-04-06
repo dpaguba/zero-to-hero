e5cfa6efbb2a922f2589a2069c19e674adc361e2
dpaguba <dpaguba@gmail.com>
2022-04-06 09:55:13 +0200

    init class

---
+++ simple-games/Tetris/Figure.py
@@ -0,0 +1,11 @@
+class Figure:
+    x = 0
+    y = 0
+    figures = [
+        [[3, 7, 11, 15], [4, 5, 6, 7]],     # I
+        [[]]
+    ]
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
